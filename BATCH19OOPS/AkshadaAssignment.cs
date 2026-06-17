6bdae9091aed0fbd9e10e52f83fe4d876a20e7db
vyavhareakshada-ai <vyavhareakshada@gmail.com>
2026-06-17 18:38:33 +0530

    Added by Akshda

---
+++ BATCH19OOPS/AkshadaAssignment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BATCH19_OOPS
+{
+    internal class AkshadaAssignment
+    {
+
+        public void test()
+        {
+            string name = "Mahesh";
+            Console.WriteLine($"name of the the clent is: {name}");
+
+
+        }     
+
+
+    
+        static void Main()
+        {
+            AkshadaAssignment obj = new AkshadaAssignment();
+            obj.test();
+
+        }     
+    }
+}
